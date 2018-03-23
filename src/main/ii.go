c9c7d4a745d4457abeac5ddf7d793c37c4460002
Dreamgoing <ruoxuanwanggoing@gmail.com>
2018-03-23 14:43:50 +0800

    fix: 删除冗余代码

--- src/main/ii.go
+++ src/main/ii.go
@@ -50,6 +50,7 @@ func reduceF(key string, values []string) string {
 
 }
 
+
 // Can be run in 3 ways:
 // 1) Sequential (e.g., go run wc.go master sequential x1.txt .. xN.txt)
 // 2) Master (e.g., go run wc.go master localhost:7777 x1.txt .. xN.txt)
